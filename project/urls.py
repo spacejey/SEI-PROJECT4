187547d0e678b2d77c693fc94629f8517c842957
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-04-15 00:38:45 +0100

    reviews complete

--- project/urls.py
+++ project/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/trucks/', include('trucks.urls')),
+    path('api/reviews/', include('reviews.urls')),
 ]
