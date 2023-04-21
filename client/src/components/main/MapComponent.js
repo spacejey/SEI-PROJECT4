0bc5b74c013a389d59295a34df29b534732818a5
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-04-21 18:09:02 +0100

    marker card

--- client/src/components/main/MapComponent.js
+++ client/src/components/main/MapComponent.js
@@ -1,8 +1,10 @@
-import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 
+// Components
+import MapMarker from './MapMarker'
 
+const libraries = ['places'] 
 const containerStyle = {
   width: '100vw',
   height: '60vh',
@@ -12,15 +14,20 @@ const containerStyle = {
   marginBottom: '50px',
 }
 
-const libraries = ['places'] 
+// User current location 
+const redCircle = {
+  path: 'M10,0A10,10,0,1,0,20,10A10,10,0,0,0,10,0Z',
+  fillColor: 'green',
+  fillOpacity: 1,
+  strokeColor: 'green',
+  strokeWeight: 0,
+  scale: 1,
+}
 
 function MapComponent() {
 
   // States
   const [currentPosition, setCurrentPosition] = useState(null)
-  const [markers, setMarkers] = useState([])
-  const [ truck, setTruck ] = useState({})
-  
 
   // Set the default location to depends on user's current lacation
   useEffect(() => {
@@ -28,51 +35,19 @@ function MapComponent() {
       (position) => {
         const { latitude, longitude } = position.coords
         setCurrentPosition({ lat: latitude, lng: longitude })
-      },
-      () => {
-        setCurrentPosition({ lat: 51.5074, lng: -0.1278 })
       }
     )
-  })
-
-  useEffect(() => {
-    const getTruck = async () => {
-      try {
-        const { data } = await axios.get('/api/trucks/')
-        setTruck(data)
-      } catch (err) {
-        console.log(err)
-      }
-    }
-    getTruck()
   }, [])
-
-  useEffect(() => {
-    axios.get('/api/trucks/')
-      .then(response => {
-        const markers = response.data.map(truck => {
-          return {
-            position: {
-              lat: truck.latitude,
-              lng: truck.longitude,
-            },
-            title: truck.name,
-          }
-        })
-        setMarkers(markers)
-      })
-      .catch(error => console.log(error))
-  }, [])
-
 
 
   return (
     <div className='map-components'>
       <LoadScript googleMapsApiKey={ process.env.REACT_APP_GOOGLE_MAPS_API_KEY } libraries={libraries}>
-        <GoogleMap mapContainerStyle={containerStyle} center={currentPosition} zoom={13}>
-          {markers.map((marker, index) => (
-            <Marker key={index} position={marker.position} title={marker.title} />
-          ))}
+        <GoogleMap mapContainerStyle={containerStyle} center={currentPosition} zoom={14}>
+          <MapMarker />
+          {currentPosition && (
+            <Marker position={currentPosition} icon={redCircle}/>
+          )}
         </GoogleMap>
       </LoadScript>
     </div>
---
+++ client/src/components/main/MapMarker.js
@@ -0,0 +1,74 @@
+import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { Marker, InfoWindow } from '@react-google-maps/api'
+import { Container } from 'react-bootstrap'
+import Geocode from 'react-geocode'
+
+
+
+function MapMarker() {
+  
+
+  // States
+  const [markers, setMarkers] = useState([])
+  const [selectedMarker, setSelectedMarker] = useState(null)
+  const [currentPosition, setCurrentPosition] = useState(null)
+  
+  
+  useEffect(() => {
+    axios.get('/api/trucks/')
+      .then(response => {
+        const markers = response.data.map(truck => {
+          return {
+            position: {
+              lat: truck.latitude,
+              lng: truck.longitude,
+            },
+            title: truck.name,
+            description: truck.description,
+            id: truck.id,
+            isOpen: false,
+          }
+        })
+        setMarkers(markers)
+      })
+      .catch(error => console.log(error))
+  }, [])
+
+  
+  function handleMarkerClick(marker) {
+    setMarkers((prevMarkers) =>
+      prevMarkers.map((m) => {
+        if (m === marker) {
+          return { ...m, isOpen: true }
+        }
+        return { ...m, isOpen: false }
+      })
+    )
+    setSelectedMarker(marker)
+  }
+  
+  return (
+    <Container>
+      {markers.map((marker) => (
+        <Marker 
+          key={marker.id}
+          position={marker.position}
+          onClick={() => handleMarkerClick(marker)}
+        >
+          {marker.isOpen  && (
+            <InfoWindow onClick={() => setSelectedMarker(null)}>
+              <div>
+                <h3>{marker.title}</h3>
+                <p>{marker.description}</p>
+              </div>
+            </InfoWindow>
+          )}
+
+        </Marker>
+      ))}
+    </Container>
+  )
+
+}
+export default MapMarker
