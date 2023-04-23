6ce577a0de3211f1cc357dbe565a1c40f0d4263e
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-04-23 16:54:49 +0100

    err

--- client/src/index.js
+++ client/src/index.js
@@ -4,4 +4,5 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/main.scss'
 import App from './App'
 
+require('dotenv').config()
 createRoot(document.getElementById('root')).render(<App />)
