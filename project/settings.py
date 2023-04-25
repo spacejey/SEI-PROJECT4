8199b24cc1d5a4fd865281a477af12e640200ec8
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-04-25 15:34:57 +0100

    deploy

--- project/settings.py
+++ project/settings.py
@@ -138,9 +138,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
