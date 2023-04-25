879cefb92e3b2f0cb96b92ed760928a3bafa3a47
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-04-25 13:39:28 +0100

    ready to deploy

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,6 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './styles/main.scss'
 import App from './App'
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
-require('dotenv').config()
 createRoot(document.getElementById('root')).render(<App />)
